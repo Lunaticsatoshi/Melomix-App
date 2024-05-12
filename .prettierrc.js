26d3126332c0a95de3be34fec4d6bcb293e44abd
Lunaticsatoshi <deysarkersudeep@gmail.com>
2024-05-13 01:40:42 +0530

    :hammer: chore: prettier and eslint config files

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,12 @@
 module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  quoteProps: 'consistent',
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
 };
