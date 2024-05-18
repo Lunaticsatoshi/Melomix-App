269d3582902a34d19a14f4957c02cbcc434859bb
Lunaticsatoshi <deysarkersudeep@gmail.com>
2024-05-19 03:03:06 +0530

    :heavy_plus_sign: chore: Adding support for ts aliases

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ["nativewind/babel"],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src/'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          "src": "./src",
+          "components": "./src/components",
+          "screens": "./src/screens",
+          "utils": "./src/utils",
+          "assets": "./src/assets",
+          "navigation": "./src/navigation",
+        }
+      }
+    ],
+    'nativewind/babel'
+  ]
 };
