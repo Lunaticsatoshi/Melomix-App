85faef489dde09f71379a4faa18370518260ba51
Lunaticsatoshi <deysarkersudeep@gmail.com>
2024-05-19 02:31:41 +0530

    :wrench: chore: updating tailwind config files with colors for nativewind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,31 +24,40 @@ module.exports = {
           DEFAULT: "hsl(0 72.2% 50.6%)",
           foreground: "hsl(0 85.7% 97.3%)",
         },
-        // secondary: {
-        //   DEFAULT: "hsl(var(--secondary))",
-        //   foreground: "hsl(var(--secondary-foreground))",
-        // },
-        // destructive: {
-        //   DEFAULT: "hsl(var(--destructive))",
-        //   foreground: "hsl(var(--destructive-foreground))",
-        // },
-        // muted: {
-        //   DEFAULT: "hsl(var(--muted))",
-        //   foreground: "hsl(var(--muted-foreground))",
-        // },
-        // accent: {
-        //   DEFAULT: "hsl(var(--accent))",
-        //   foreground: "hsl(var(--accent-foreground))",
-        // },
+        secondary: {
+          DEFAULT: "hsl(0 0% 96.1%)",
+          dark: "hsl(215 21% 18%)",
+          foreground: "hsl(0 0% 9%)",
+          foregroundDark: "hsl(0 0% 98%)",
+        },
+        destructive: {
+          DEFAULT: "hsl(0 84.2% 60.2%)",
+          dark: "hsl(0 62.8% 30.6%)",
+          foreground: "hsl(0 0% 98%)",
+        },
+        muted: {
+          DEFAULT: "hsl(0 0% 96.1%)",
+          dark: "hsl(0 0% 14.9%)",
+          foreground: "hsl(0 0% 45.1%)",
+          foregroundDark: "hsl(0 0% 63.9%)",
+        },
+        accent: {
+          DEFAULT: "hsl(0 0% 96.1%)",
+          dark: "hsl(0 0% 14.9%)",
+          foreground: "hsl(0 0% 9%)",
+          foregroundDark: "hsl(0 0% 63.9%)",
+        },
         // popover: {
         //   DEFAULT: "hsl(var(--popover))",
         //   foreground: "hsl(var(--popover-foreground))",
         // },
-        // card: {
-        //   DEFAULT: "hsl(var(--card))",
-        //   foreground: "hsl(var(--card-foreground))",
-        //   border: "hsl(var(--card-border))",
-        // },
+        card: {
+          DEFAULT: "hsl(0 0% 100%)",
+          dark: "hsl(215 22% 11%)",
+          foreground: "hsl(0 0% 3.9%)",
+          foregroundDark: "hsl(0 0% 98%)",
+          border: "hsl(215 22% 11%)",
+        },
       },
       borderRadius: {
         lg: "0.5rem",
